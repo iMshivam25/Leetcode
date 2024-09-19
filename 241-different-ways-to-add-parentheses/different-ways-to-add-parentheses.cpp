2b633089eb7bd3e8bd0bee39e6c9510e5ecffcbb
Shivam Mishra <shivam.mishra.alld@gmail.com>
2024-09-19 18:26:05 +0530

    Time: 4 ms (46.04%) | Memory: 13.8 MB (40.75%) - LeetSync

---
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string expression) {
+        vector<int> ans;
+        for (int i=0;i<expression.size();i++){
+            char ch = expression[i];
+            if (ch=='+'||ch=='-'||ch=='*'){
+                vector<int>left,right;
+                left = diffWaysToCompute(expression.substr(0,i));
+                right = diffWaysToCompute(expression.substr(i+1));
+            
+                for (auto x:left){
+                    for (auto y:right){
+                        if (ch=='+') ans.push_back(x+y);
+                        else if (ch=='-') ans.push_back(x-y);
+                        else ans.push_back(x*y);
+                    }
+                }
+            }
+        }
+    if (ans.empty()) ans.push_back(stoi(expression));
+    return ans;
+    }
+};
