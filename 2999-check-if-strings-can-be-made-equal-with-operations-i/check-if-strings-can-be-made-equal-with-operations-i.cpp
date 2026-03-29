bb42579edd3943c2eed2065b45def71ad23231b6
Shivam Mishra <shivam.mishra.alld@gmail.com>
2026-03-29 15:05:37 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (49.03%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool canBeEqual(string s1, string s2) {
+        bool ans = true;
+        // if(s1[0]==s2[0] && s1[1]==s2[1]){
+        //     if(s1[2]!=s2[2] || s1[3]!=s2[3]) return false;
+        // }
+        if (s1[0]!=s2[0]){
+            ans = (s1[2]==s2[0] && s1[0]==s2[2]);
+            s1[2]=s1[0];
+        }
+        if (s1[1]!=s2[1] && ans){
+            ans = (s1[3]==s2[1] && s1[1]==s2[3]);
+            s1[3]=s1[1];
+        }
+        if (s1[2]!=s2[2] || s1[3]!=s2[3]) return false;
+        return ans;
+    }
+};
